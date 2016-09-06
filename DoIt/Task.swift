1d9fe8a46199788adae3056058167100f1b9c7dd
Mago Nicolas Palacios <magonicolas@gmail.com>
2016-09-06 17:24:37 -0300

    Main Logic Added, and listend tasks

---
+++ DoIt/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Mago Nicolas Palacios on 9/6/16.
+//  Copyright © 2016 Mago Nicolas Palacios. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    var important = false
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,16 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if !task.important {
+            cell.textLabel?.text = task.name
+        } else {
+            cell.textLabel?.text = "\(task.name) ❗️"
+        }
+        return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Learn iOs"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Master iOs"
+        task2.important = true
+
+        
+        let task3 = Task()
+        task3.name = "Teach iOs"
+        task3.important = true
+        
+        return [task1, task2, task3]
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func plusTapped(_ sender: AnyObject) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
     }
 
 
